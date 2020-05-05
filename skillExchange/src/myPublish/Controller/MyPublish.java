78e6d493ed422c3c7a347a514c5a4f67a9659a43
JamieHu0426 <64365841+JamieHu0426@users.noreply.github.com>
2020-05-05 18:49:12 +0800

    Update MyPublish.java
    test0649

--- skillExchange/src/myPublish/Controller/MyPublish.java
+++ skillExchange/src/myPublish/Controller/MyPublish.java
@@ -37,7 +37,6 @@ public class MyPublish extends HttpServlet {
 	private void processRequest(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 //        test2
-		//test jamie
 		request.setCharacterEncoding("UTF-8");
 		httpSession = request.getSession();
 		List<MyPublishBean> mylist;
